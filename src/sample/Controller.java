5bda619ce26f8fa52fe064635326efce13317383
Igor <igor.spiridonov.99@mail.ru>
2017-01-07 20:12:11 +0300

    sadasa

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,50 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @FXML
+    private HBox secondPanel;
+    @FXML
+    private HBox firstPanel;
+    @FXML
+    private HBox thirdPanel;
+
+    public void addTeapot() {
+
+    }
+
+    public void removeTeapot() {
+
+    }
+
+
+    public void addBulb() {
+
+    }
+
+
+    public void removeBulb() {
+
+    }
+
+    public void addComputer() {
+
+    }
+
+
+    public void removeComputer() {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Counter");
-        primaryStage.setScene(new Scene(root, 580, 500));
+        primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
